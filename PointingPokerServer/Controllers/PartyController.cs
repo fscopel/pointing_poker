7bd0a14853b35688b04926a6f6eb4133afa45866
Fabio Scopel <fabioscopel99@gmail.com>
2021-03-21 22:48:14 -0500

    Server code

---
+++ PointingPokerServer/Controllers/PartyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointingPokerServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartyController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            var response = Enumerable.Range(1, 34).ToList();
+            return response;
+        }
+    }
+}
---
+++ PointingPokerServer/SignalRChat.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointingPokerServer
+{
+    public class SignalRChat : Hub
+    {
+        public async Task JoinRoom(string userName, string roomName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", userName, $"Joined Room: {roomName}");
+        }
+
+        public async Task LeaveRoom(string roomName)
+        { 
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public async Task SendMessage(string user, string message, string roomName)
+        {
+            try
+            {
+                await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
+            }
+            catch (Exception e)
+            {
+
+                
+            }
+        }
+    }
+}
